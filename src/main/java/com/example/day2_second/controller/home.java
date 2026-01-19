bf39ea9f5a950c3f87bdcc0ffe1aa98fdd7f6d36
demorepo <tanmayagrawal278@gmail.com>
2026-01-19 15:36:57 +0530

    return indexed of object

--- src/main/java/com/example/day2_second/controller/home.java
+++ src/main/java/com/example/day2_second/controller/home.java
@@ -2,6 +2,7 @@ package com.example.day2_second.controller;
 
 import com.example.day2_second.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,6 +23,11 @@ public class home {
         arr.add(student4);
         arr.add(student5);
         return arr;
+    }
+    @GetMapping("/{id}")
+    public StudentModel getstudentbyid(@PathVariable int id){
+        if(id<1 || id>5) return new StudentModel(1,null,null,null);
+        return getStudent().get(id-1);
     }
     public String about(){
         return "This is the about page";
